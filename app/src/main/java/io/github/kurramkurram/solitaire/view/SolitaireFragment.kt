3d0f02dd8c2ba44260f2182fba84adba5fbbf32b
Masataka.Goto <54581188+kurramkurram@users.noreply.github.com>
2023-01-05 21:29:28 +0900

    [Update] Create Fragment layout file

--- app/src/main/java/io/github/kurramkurram/solitaire/view/SolitaireFragment.kt
+++ app/src/main/java/io/github/kurramkurram/solitaire/view/SolitaireFragment.kt
@@ -1,17 +1,33 @@
 package io.github.kurramkurram.solitaire.view
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import io.github.kurramkurram.solitaire.R
+import io.github.kurramkurram.solitaire.util.L
 import io.github.kurramkurram.solitaire.viewmodel.SolitaireViewModel
 
 class SolitaireFragment : Fragment() {
 
     private val solitaireViewModel by viewModels<SolitaireViewModel>()
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_solitaire, container, false)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        
+        L.d(TAG, "#onViewCreated")
+    }
+
+    companion object {
+        private const val TAG = "SolitaireFragment"
     }
 }
