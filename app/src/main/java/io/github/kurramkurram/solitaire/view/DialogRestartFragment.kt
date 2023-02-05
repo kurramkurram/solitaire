e7113c33871cc3b891d352ee0a8aa55f43ba629f
Masataka.Goto <54581188+kurramkurram@users.noreply.github.com>
2023-02-05 21:43:39 +0900

    [Update] string define about DialogRestartFragment

--- app/src/main/java/io/github/kurramkurram/solitaire/view/DialogRestartFragment.kt
+++ app/src/main/java/io/github/kurramkurram/solitaire/view/DialogRestartFragment.kt
@@ -3,7 +3,9 @@ package io.github.kurramkurram.solitaire.view
 import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.DialogFragment
+import io.github.kurramkurram.solitaire.R
 import io.github.kurramkurram.solitaire.util.*
 
 class DialogRestartFragment : DialogFragment() {
@@ -12,23 +14,19 @@ class DialogRestartFragment : DialogFragment() {
 
         val alertDialog = AlertDialog.Builder(activity).apply {
             val data = Bundle()
-
-            setTitle("リセット")
-            setMessage("リセットしますか？")
-            setPositiveButton("OK") { _, _ ->
+            setTitle(resources.getString(R.string.restart_dialog_title))
+            setMessage(resources.getString(R.string.restart_dialog_text))
+            setIcon(ResourcesCompat.getDrawable(resources, R.drawable.restart_dark, null))
+            setPositiveButton(resources.getString(R.string.restart_dialog_positive)) { _, _ ->
                 data.putInt(DIALOG_RESULT_KEY, DIALOG_RESULT_OK)
                 parentFragmentManager.setFragmentResult(DIALOG_RESULT, data)
             }
-            setNegativeButton("No") { _, _ ->
+            setNegativeButton(resources.getString(R.string.restart_dialog_negative)) { _, _ ->
                 data.putInt(DIALOG_RESULT_KEY, DIALOG_RESULT_NG)
                 parentFragmentManager.setFragmentResult(DIALOG_RESULT, data)
             }
         }.create()
 
         return alertDialog
-    }
-
-    companion object {
-        const val TAG = "RestartFragment"
     }
 }
