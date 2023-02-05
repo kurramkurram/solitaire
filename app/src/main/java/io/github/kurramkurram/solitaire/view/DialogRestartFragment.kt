c4d0bf97b638011d79e627b0df9cf33215631192
Masataka.Goto <54581188+kurramkurram@users.noreply.github.com>
2023-02-06 00:03:01 +0900

    [Update] Dialog design

--- app/src/main/java/io/github/kurramkurram/solitaire/view/DialogRestartFragment.kt
+++ app/src/main/java/io/github/kurramkurram/solitaire/view/DialogRestartFragment.kt
@@ -1,32 +1,35 @@
 package io.github.kurramkurram.solitaire.view
 
-import android.app.AlertDialog
 import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.DialogFragment
 import io.github.kurramkurram.solitaire.R
 import io.github.kurramkurram.solitaire.util.*
+import kotlinx.android.synthetic.main.dialog_restart.*
 
 class DialogRestartFragment : DialogFragment() {
 
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog =
+        Dialog(requireContext()).apply {
+            setContentView(R.layout.dialog_restart)
+            window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            positive_button.setOnClickListener { onPositiveClick() }
+            negative_button.setOnClickListener { onNegativeClick() }
+        }
 
-        val alertDialog = AlertDialog.Builder(activity).apply {
-            val data = Bundle()
-            setTitle(resources.getString(R.string.restart_dialog_title))
-            setMessage(resources.getString(R.string.restart_dialog_text))
-            setIcon(ResourcesCompat.getDrawable(resources, R.drawable.restart_dark, null))
-            setPositiveButton(resources.getString(R.string.restart_dialog_positive)) { _, _ ->
-                data.putInt(DIALOG_RESULT_KEY, DIALOG_RESULT_OK)
-                parentFragmentManager.setFragmentResult(DIALOG_RESULT, data)
-            }
-            setNegativeButton(resources.getString(R.string.restart_dialog_negative)) { _, _ ->
-                data.putInt(DIALOG_RESULT_KEY, DIALOG_RESULT_NG)
-                parentFragmentManager.setFragmentResult(DIALOG_RESULT, data)
-            }
-        }.create()
+    private fun onPositiveClick() {
+        val data = Bundle()
+        data.putInt(DIALOG_RESULT_KEY, DIALOG_RESULT_OK)
+        parentFragmentManager.setFragmentResult(DIALOG_RESULT, data)
+        dismiss()
+    }
 
-        return alertDialog
+    private fun onNegativeClick() {
+        val data = Bundle()
+        data.putInt(DIALOG_RESULT_KEY, DIALOG_RESULT_NG)
+        parentFragmentManager.setFragmentResult(DIALOG_RESULT, data)
+        dismiss()
     }
 }
