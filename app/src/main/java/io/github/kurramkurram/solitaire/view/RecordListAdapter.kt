5ed1908344744742a574cff8e0bf9f4e8f14d7c6
Masataka.Goto <54581188+kurramkurram@users.noreply.github.com>
2023-02-12 00:32:49 +0900

    [Update] refresh all record list

--- app/src/main/java/io/github/kurramkurram/solitaire/view/RecordListAdapter.kt
+++ app/src/main/java/io/github/kurramkurram/solitaire/view/RecordListAdapter.kt
@@ -1,26 +1,20 @@
 package io.github.kurramkurram.solitaire.view
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.lifecycle.LifecycleOwner
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import io.github.kurramkurram.solitaire.data.Record
 import io.github.kurramkurram.solitaire.databinding.RecordItemBinding
 import io.github.kurramkurram.solitaire.viewmodel.RecordViewModel
 
-private object RecordDiffCallback : DiffUtil.ItemCallback<Record>() {
-    override fun areItemsTheSame(oldItem: Record, newItem: Record): Boolean =
-        oldItem.id == newItem.id
-
-    override fun areContentsTheSame(oldItem: Record, newItem: Record): Boolean = oldItem == newItem
-}
-
 class RecordListAdapter(
     private val viewLifecycleOwner: LifecycleOwner,
     private val viewModel: RecordViewModel
-) : ListAdapter<Record, RecordListAdapter.RecordViewHolder>(RecordDiffCallback) {
+) : RecyclerView.Adapter<RecordListAdapter.RecordViewHolder>() {
+
+    private val list: MutableList<Record> = mutableListOf()
 
     class RecordViewHolder(private val binding: RecordItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
@@ -48,6 +42,15 @@ class RecordListAdapter(
     }
 
     override fun onBindViewHolder(holder: RecordViewHolder, position: Int) {
-        holder.bind(getItem(position), position, viewLifecycleOwner, viewModel)
+        holder.bind(list[position], position, viewLifecycleOwner, viewModel)
+    }
+
+    override fun getItemCount(): Int = list.size
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun submitList(items: List<Record>) {
+        list.clear()
+        list.addAll(items)
+        notifyDataSetChanged()
     }
 }
