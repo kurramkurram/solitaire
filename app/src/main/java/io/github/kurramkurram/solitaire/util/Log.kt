2564626ab338a692bee5510e6207ec9c6b98d79a
Masataka.Goto <54581188+kurramkurram@users.noreply.github.com>
2023-02-19 23:20:19 +0900

    [Add] Build config

--- app/src/main/java/io/github/kurramkurram/solitaire/util/Log.kt
+++ app/src/main/java/io/github/kurramkurram/solitaire/util/Log.kt
@@ -1,15 +1,20 @@
 package io.github.kurramkurram.solitaire.util
 
 import android.util.Log
+import io.github.kurramkurram.solitaire.BuildConfig
 
 class L {
     companion object {
         fun d(tag: String, msg: String) {
-            Log.d("Solitaire", "$tag$msg")
+            if (BuildConfig.DEBUG) {
+                Log.d("Solitaire", "$tag$msg")
+            }
         }
 
         fun e(tag: String, msg: String) {
-            Log.e("Solitaire", "$tag$msg")
+            if (BuildConfig.DEBUG) {
+                Log.e("Solitaire", "$tag$msg")
+            }
         }
     }
 }
