2e09d03a77ab14daf4c37e4360139c195d67d462
Masataka.Goto <54581188+kurramkurram@users.noreply.github.com>
2023-03-10 23:02:20 +0900

    [Add] auto complete dialog

---
+++ app/src/main/java/io/github/kurramkurram/solitaire/view/DialogAutoCompleteFragment.kt
@@ -0,0 +1,34 @@
+package io.github.kurramkurram.solitaire.view
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.activityViewModels
+import io.github.kurramkurram.solitaire.R
+import io.github.kurramkurram.solitaire.databinding.DialogAutoCompleteBinding
+import io.github.kurramkurram.solitaire.util.DIALOG_RESULT_AUTO_COMPLETE
+import io.github.kurramkurram.solitaire.viewmodel.SolitaireViewModel
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.dialog_auto_complete.*
+
+class DialogAutoCompleteFragment : DialogBaseFragment() {
+
+    private val solitaireViewModel: SolitaireViewModel by activityViewModels()
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val binding = DialogAutoCompleteBinding.inflate(layoutInflater, container, false).apply {
+            this.viewModel = solitaireViewModel
+        }
+
+        return Dialog(requireContext()).apply {
+            setContentView(binding.root)
+            window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            positive_button.setOnClickListener { onPositiveClick(DIALOG_RESULT_AUTO_COMPLETE) }
+            negative_button.setOnClickListener { onNegativeClick(DIALOG_RESULT_AUTO_COMPLETE) }
+        }
+    }
+}
+
