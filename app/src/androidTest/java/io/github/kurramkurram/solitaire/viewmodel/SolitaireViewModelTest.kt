fc42959f112744fd7e95c44fbf045822a249d623
Masataka.Goto <54581188+kurramkurram@users.noreply.github.com>
2023-11-30 23:40:31 +0900

    [Update] Android 14

--- app/src/androidTest/java/io/github/kurramkurram/solitaire/viewmodel/SolitaireViewModelTest.kt
+++ app/src/androidTest/java/io/github/kurramkurram/solitaire/viewmodel/SolitaireViewModelTest.kt
@@ -1,11 +1,14 @@
 package io.github.kurramkurram.solitaire.viewmodel
 
+import android.app.Application
+import androidx.lifecycle.MutableLiveData
 import io.github.kurramkurram.solitaire.data.TrumpCard
 import io.github.kurramkurram.solitaire.util.L
 import io.github.kurramkurram.solitaire.util.NUMBER
 import io.github.kurramkurram.solitaire.util.PATTERN
 import io.github.kurramkurram.solitaire.util.SIDE
 import org.junit.Test
+import org.mockito.Mockito
 
 class SolitaireViewModelTest {
 
@@ -14,13 +17,12 @@ class SolitaireViewModelTest {
         val initList = mutableListOf<TrumpCard>()
         PATTERN.values().forEach { pattern ->
             NUMBER.values().forEach { number ->
-                initList.add(TrumpCard(number, pattern, SIDE.BACK))
+                initList.add(TrumpCard(number, pattern, MutableLiveData(SIDE.BACK)))
             }
         }
 
-        val solitaireViewModel = SolitaireViewModel()
+        val solitaireViewModel = SolitaireViewModel(Mockito.mock(Application::class.java))
         val list = solitaireViewModel.createLayout(initList)
-        L.d(TAG, "#testCreateLayout = " + list.size)
         L.d(TAG, "#testCreateLayout = $list")
     }
 
