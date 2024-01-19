515440722c033d7ca54749d1792c1db33439ae50
GM <54581188+kurramkurram@users.noreply.github.com>
2024-01-19 21:36:47 +0900

    [Fix] Average Data (#41)

--- app/src/main/java/io/github/kurramkurram/solitaire/view/GraphFragment.kt
+++ app/src/main/java/io/github/kurramkurram/solitaire/view/GraphFragment.kt
@@ -8,9 +8,12 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
 import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
 import io.github.kurramkurram.solitaire.R
+import io.github.kurramkurram.solitaire.data.BarChartData
+import io.github.kurramkurram.solitaire.data.PieChartData
 import io.github.kurramkurram.solitaire.databinding.FragmentGraphBinding
 import io.github.kurramkurram.solitaire.util.CHART_SUCCESS_COUNT_BAR
 import io.github.kurramkurram.solitaire.util.CHART_SUCCESS_TIME_PIE
@@ -24,6 +27,14 @@ class GraphFragment : Fragment() {
 
     private lateinit var binding: FragmentGraphBinding
     private val graphViewModel: GraphViewModel by activityViewModels()
+
+    private val barChartDataObserver = Observer<MutableList<BarChartData>?> {
+        it?.let { graphViewModel.createBarChartData(it) }
+    }
+
+    private val pieChartDataObserver = Observer<MutableList<PieChartData>?> {
+        it?.let { graphViewModel.createPieChartData(it) }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -57,6 +68,8 @@ class GraphFragment : Fragment() {
     }
 
     private fun setSuccessCountBarChart() {
+        removeObserver()
+
         graphViewModel.run {
             barData.observe(viewLifecycleOwner) {
                 binding.barChart.apply {
@@ -85,13 +98,13 @@ class GraphFragment : Fragment() {
                     invalidate()
                 }
             }
-            barChartData.observe(viewLifecycleOwner) {
-                graphViewModel.createBarChartData(it)
-            }
+            barChartData.observe(viewLifecycleOwner, barChartDataObserver)
         }
     }
 
     private fun setSuccessTimePieChart() {
+        removeObserver()
+
         graphViewModel.run {
             pieData.observe(viewLifecycleOwner) {
                 binding.pieChart.apply {
@@ -100,9 +113,14 @@ class GraphFragment : Fragment() {
                     invalidate()
                 }
             }
-            pieChartData.observe(viewLifecycleOwner) {
-                graphViewModel.createPieChartData(it)
-            }
+            pieChartData.observe(viewLifecycleOwner, pieChartDataObserver)
+        }
+    }
+
+    private fun removeObserver() {
+        graphViewModel.run {
+            barChartData.removeObserver(barChartDataObserver)
+            pieChartData.removeObserver(pieChartDataObserver)
         }
     }
 
